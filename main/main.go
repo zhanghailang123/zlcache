72fa4ee857f2d003134d217f9491404bafccc493
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-10-17 22:32:09 +0800

    feature:测试使用sync.Mutex

--- main/main.go
+++ main/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"container/list"
 	"fmt"
+	"sync"
+	"time"
 )
 
-func main() {
+func main1() {
 
 	l := list.New()
 
@@ -39,4 +41,24 @@ func main() {
 		}
 	}()
 
+}
+
+var m sync.Mutex
+
+var set = make(map[int]bool, 0)
+
+func printOnce(num int) {
+	m.Lock()
+	if _, exist := set[num]; exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+	m.Unlock()
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(time.Second)
 }

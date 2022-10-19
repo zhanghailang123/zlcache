0b2619a7a96fa5bb98c88d7a0ad1eb69b2bd050c
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-10-19 21:59:19 +0800

    feature:暂提代码

--- zcache/zcache.go
+++ zcache/zcache.go
@@ -1,17 +1,17 @@
 package zcache
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 type Getter interface {
 	Get(key string) ([]byte, error)
 }
 
-//函数式接口
+// GetterFunc 函数式接口
 type GetterFunc func(key string) ([]byte, error)
-
-func (f GetterFunc) Get(key string) ([]byte, error) {
-	return f(key)
-}
 
 type Group struct {
 	name      string
@@ -19,7 +19,65 @@ type Group struct {
 	mainCache cache
 }
 
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
 var (
 	mu     sync.RWMutex
 	groups = make(map[string]*Group)
 )
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[Zcache] hit")
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
--- zcache/zcache_test.go
+++ zcache/zcache_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 /**
-
- */
+ww
+*/
 func TestGetterFunc_Get(t *testing.T) {
 	//将一个匿名回调函数转换成接口
 	var f Getter = GetterFunc(func(key string) ([]byte, error) {

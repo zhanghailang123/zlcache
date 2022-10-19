5e3e9020eeb3c8e870482a6b28748a921dfe08bb
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-10-19 22:45:51 +0800

    feature:暂时提交一致性hash算法

---
+++ zcache/consistenthash/consistenthash.go
@@ -0,0 +1,25 @@
+package consistenthash
+
+import "hash/crc32"
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int
+	hashMap  map[int]string
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		hash:     fn,
+		replicas: replicas,
+		hashMap:  make(map[int]string),
+	}
+
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}

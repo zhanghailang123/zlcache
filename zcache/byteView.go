dedd47b1a79d0a942d62592e4a7fdd5c86ecd70c
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-10-21 18:59:26 +0800

    feature：单元测试及代码优化

--- zcache/byteView.go
+++ zcache/byteView.go
@@ -11,6 +11,9 @@ func (v ByteView) Len() int {
 func (v ByteView) ByteSlice() []byte {
 	return cloneBytes(v.b)
 }
+func (v ByteView) String() string {
+	return string(v.b)
+}
 
 func cloneBytes(b []byte) []byte {
 	c := make([]byte, len(b))

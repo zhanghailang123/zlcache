41f0370904620b5e8d3980ccf67012945a9e044b
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-10-21 17:49:23 +0800

    feature：一致性hash算法及单元测试提交

--- zcache/consistenthash/consistenthash.go
+++ zcache/consistenthash/consistenthash.go
@@ -1,6 +1,10 @@
 package consistenthash
 
-import "hash/crc32"
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
 
 type Hash func(data []byte) uint32
 
@@ -22,4 +26,34 @@ func New(replicas int, fn Hash) *Map {
 		m.hash = crc32.ChecksumIEEE
 	}
 	return m
+}
+
+//keys : 真实节点地址的集合
+func (m *Map) Add(keys ...string) {
+	//循环添加key
+	for _, key := range keys {
+		//虚拟节点数量 每个节点都得添加这么多
+		for i := 0; i < m.replicas; i++ {
+			//i + 节点地址 调用hash函数 生成hash码
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			//keys + hash节点的地址 往虚拟节点数组集合里面添加刚刚生成的hash码（）
+			m.keys = append(m.keys, hash)
+			//虚拟节点与真实节点的映射表
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	//调用同一hash 函数对key计算hash
+	hash := int(m.hash([]byte(key)))
+
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	return m.hashMap[m.keys[idx%len(m.keys)]]
 }

d1acde7b99a4aaac741b2dbc6f4c5aa9af85694f
hailang.zhang <hailang.zhang@zhaopin.com.cn>
2022-10-26 22:05:46 +0800

    feature:防止缓存击穿

--- zcache/zcache.go
+++ zcache/zcache.go
@@ -1,6 +1,7 @@
 package zcache
 
 import (
+	"ZCache/zcache/singleflight"
 	"fmt"
 	"log"
 	"sync"
@@ -18,6 +19,7 @@ type Group struct {
 	getter    Getter
 	mainCache cache
 	peers     PeerPicker
+	loader    *singleflight.Group
 }
 
 func (g *Group) RegisterPeers(peers PeerPicker) {
@@ -46,6 +48,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -72,15 +75,23 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err := g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err := g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[zcache]:Failed to get from peer", err)
 			}
-			log.Println("[zcache]:Failed to get from peer", err)
 		}
+		return g.getLocally(key)
+	})
+
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
-	return g.getLocally(key)
+
+	return
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
